e95423dbb42e91d9b4e17b66c2c7c41d4bbb34b5
CASPARBOETES <CASPARBOETES@GMAIL.COM>
2018-01-05 14:42:20 +0100

    Add algo, styling

--- app/models/pair.rb
+++ app/models/pair.rb
@@ -1,4 +1,4 @@
-# require_relative 'user.rb'
+require_relative 'user.rb'
 
 class Pair
    attr_reader :days
@@ -22,18 +22,16 @@ class Pair
     end
   end
 
-
   private
 
   def pairing_up
-    @students_array.push(nil) if @students_array.length.odd?
-    days = 1
-    number_of_days = @students_array.length / 2
+    @students_array.push('Pick any group you like!') if @students_array.size.odd?
+    number_of_todays_pairs = @students_array.size / 2
 
-    days.times do |index|
-      @days[index+1] = []
-      number_of_days.times do |number_index|
-        @days[index+1] << [@students_array[number_index], @students_array.reverse[number_index]]
+    1.times do |index|
+      @days[index] = []
+      number_of_todays_pairs.times do |number_index|
+        @days[index] << [@students_array[number_index], @students_array.reverse[number_index]]
       end
       @students_array = [@students_array[0]] + @students_array[1..-1].rotate(-1)
     end
